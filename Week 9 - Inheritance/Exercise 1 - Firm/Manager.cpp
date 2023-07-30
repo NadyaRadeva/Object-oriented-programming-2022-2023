7c33599299140327d848411f9ad318fba1d04ea9
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-07-30 06:40:30 +0300

    Create Manager.cpp

---
+++ Week 9 - Inheritance/Exercise 1 - Firm/Manager.cpp
@@ -0,0 +1,28 @@
+﻿#include "Manager.h"
+
+Manager::Manager(const std::string& name, double salary,
+	std::vector<TeamLeader> leaders1) :
+	Employee(name, salary), leaders(leaders1) {
+}
+
+double Manager::getExpenses() {
+	double value = getSalary();
+	for (int i = 0; i < leaders.size(); i++) {
+		value += leaders[i].getExpenses();
+  }
+
+	return value;
+}
+
+std::ostream& operator<<(std::ostream& str, const Manager& obj) {
+	const Employee& ref = obj; 
+
+	str << "Manager: ";
+	str << ref; // така извикваме версията на << която работи с обект Employee
+	str << "Projects: " << std::endl;
+	for (int i = 0; i < obj.leaders.size(); i++) {
+		str << obj.leaders[i];
+	}
+
+	return str;
+}
