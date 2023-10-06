02ab73295d3ed3e46602c9b09f38d2edb7c91be7
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 10:59:21 +0300

    Create Hogwarts.h

---
+++ Week 7 - Classes and files/HogwartsBattles/Hogwarts.h
@@ -0,0 +1,33 @@
+#ifndef HOGWARTS_H
+#define HOGWARTS_H
+
+#include "Student.h"
+
+class Hogwarts {
+ public:
+  Hogwarts();
+  Hogwarts(const Hogwarts& other);
+  Hogwarts(Hogwarts&& other);
+  Hogwarts& operator=(const Hogwarts& other);
+  Hogwarts& operator=(Hogwarts&& other) noexcept;
+  ~Hogwarts();
+
+  friend std::ostream& operator<<(std::ostream& out, const Hogwarts& hogwarts);
+
+  void addStudent(const Student& student);
+  void assignHouse(const std::string& studentName, const std::string& house);
+  void givePower(const std::string& name, const Power& power);
+  int getHouseStudentsCount(const std::string& house) const;
+  Student* getFirstStudent(const std::string& house);
+  const Student* getStudents() const;
+  bool removeStudent(const std::string& name);
+
+ private:
+  void clear();
+
+  Student* students_;
+  int numStudents_;
+  int capacity_;
+};
+
+#endif  // HOGWARTS_H
