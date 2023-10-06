010a4789ac74645ab8a891147e61989b6d26198e
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 14:46:42 +0300

    Create QuestionFactory.h

---
+++ Week 14 - Factory Design Pattern/Exercise - Grading/QuestionFactory.h
@@ -0,0 +1,41 @@
+#include "YesNoQuestion.h"
+#include "MultipleChoiceQuestion.h"
+#include "OpenQuestion.h"
+
+#ifndef QUESTIONFACTORY_H_
+#define QUESTIONFACTORY_H_
+
+class QuestionFactory {
+public:
+	Question* createQuestion();
+	/*
+	 Примерна имплментация в cpp файла:
+	
+	{
+		std::string type;
+		std::getline(std::cin, type);
+		if (type == "YesNo")
+		{
+			std::string question,ans;
+			double points;
+			std::cout << "Write question: \n";
+			std::getline(std::cin, question);
+			std::cout << "Write correct answer: \n";
+			std::getline(std::cin, ans);
+
+			std::cout << "Write points: \n";
+			std::cin >> points;
+
+			if (Question::validYesNoAnswer(ans))
+				if (ans == Question::YES)
+					return new YesNoQuestion(question, points, true);
+				else
+					return new YesNoQuestion(question, points, false);
+			else
+				return nullptr;
+		}
+		else if ....
+	*/
+};
+
+#endif
