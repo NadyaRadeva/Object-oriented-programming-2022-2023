9f1cc7ca9ec5dcce9082a3aa94218734adea0acc
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 14:43:23 +0300

    Create MultipleChoiceQuestion.h

---
+++ Week 14 - Factory Design Pattern/Exercise - Grading/MultipleChoiceQuestion.h
@@ -0,0 +1,33 @@
+#include <vector>
+#include "Question.h"
+
+#ifndef MULTIPLECHOICEQUESTION_H_
+#define MULTIPLECHOICEQUESTION_H_
+
+class MultipleChoiceQuestion : public Question {
+    int n;
+    std::vector<bool> correct_is_yes;
+    std::vector<std::string> statements;
+
+    std::vector<std::string> student_answers;
+
+public:
+    MultipleChoiceQuestion(const std::string& question1, double points1,
+        std::vector<std::string> statements1, std::vector<bool> correct_is_yes);
+    /*
+        n = std::min( statements1.size(), correct_is_yes.size() )
+        statements1.resize(n);
+        correct_is_yes.resize(n);
+        така се скъсява по-големия вектор
+
+        така правим вектор от n празни низа:
+        student_answers.resize(n); - възможно е въпроса да се зададе няколко пъти
+        и затова не е удачно да се ползва push_back
+    */
+
+    void ask();
+    double grade();
+    void print();
+};
+
+#endif
