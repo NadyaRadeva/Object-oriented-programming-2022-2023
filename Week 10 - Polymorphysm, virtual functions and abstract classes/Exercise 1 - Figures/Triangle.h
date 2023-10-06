58bf6c510d794f5d462530ab5eb246bdfc36f1e6
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 10:37:04 +0300

    Create Triangle.h

---
+++ Week 10 - Polymorphysm, virtual functions and abstract classes/Exercise 1 - Figures/Triangle.h
@@ -0,0 +1,18 @@
+#include "Shape.h"
+
+#ifndef TRIANGLE_H_
+#define TRIANGLE_H_
+
+class Triangle : public Shape {
+private:
+	double side_a, side_b, side_c;
+
+public:
+	Triangle(const std::string& label1, double side_a1,double,double);
+
+	double getPerimeter();
+	void print();
+	Shape* copy() const;
+};
+
+#endif
