08b2b5557b8eef76705392409e4905ded0628476
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 13:51:04 +0300

    Create Source.cpp

---
+++ Week 7 - Classes and files/HogwartsBattles/Source.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <string>
+
+#include "Hogwarts.h"
+#include "Power.h"
+#include "Student.h"
+
+int main() {
+  Power spell("Expelliarmus", 10);
+  Power curse("Cruciatus", 20);
+  Power charm("Lumos", 5);
+
+  Hogwarts hogwarts;
+
+  hogwarts.addStudent(Student("Harry Potter"));
+  hogwarts.addStudent(Student("Ron Weasley"));
+  hogwarts.addStudent(Student("Hermione Granger"));
+  hogwarts.addStudent(Student("Draco Malfoy"));
+  hogwarts.addStudent(Student("Luna Lovegood"));
+
+  hogwarts.assignHouse("Harry Potter", "Gryffindor");
+  hogwarts.assignHouse("Ron Weasley", "Gryffindor");
+  hogwarts.assignHouse("Hermione Granger", "Gryffindor");
+  hogwarts.assignHouse("Draco Malfoy", "Slytherin");
+  hogwarts.assignHouse("Luna Lovegood", "Ravenclaw");
+
+  hogwarts.givePower("Harry Potter", spell);
+  hogwarts.givePower("Ron Weasley", spell);
+  hogwarts.givePower("Hermione Granger", curse);
+  hogwarts.givePower("Draco Malfoy", curse);
+  hogwarts.givePower("Luna Lovegood", charm);
+
+  std::cout << "Students in Hogwarts: \n" << hogwarts << std::endl;
+
+  // Fight!
+  while (hogwarts.getHouseStudentsCount("Gryffindor") > 0 &&
+         hogwarts.getHouseStudentsCount("Slytherin") > 0) {
+    // Choose two random students from different houses
+    Student* student1 = nullptr;
+    Student* student2 = nullptr;
+    while (student1 == nullptr || student2 == nullptr ||
+           student1->getHouse() == student2->getHouse()) {
+      student1 = hogwarts.getFirstStudent("Gryffindor");
+      student2 = hogwarts.getFirstStudent("Slytherin");
+    }
+    std::cout << "Fight between " << student1->getName() << " (Gryffindor) and "
+              << student2->getName() << " (Slytherin)" << std::endl;
+    
+    while (true) {
+      if (!student1->isAlive()) {
+        std::cout << student2->getName() << " (Slytherin) wins!" << std::endl;
+        hogwarts.removeStudent(student1->getName());
+        student1 = nullptr;
+        break;
+      }
+      
+      int damage = student1->attack();
+      if (damage > 0) {
+        student2->takeDamage(damage);
+        std::cout << student1->getName() << " [" << student1->getHealth()
+                  << "] "
+                  << "(Gryffindor) attacks " << student2->getName()
+                  << " (Slytherin) with " << student1->getPower()->getName()
+                  << " for " << damage << " damage" << std::endl;
+      }
+
+      // Check if student 2 is still alive
+      if (!student2->isAlive()) {
+        std::cout << student1->getName() << " (Gryffindor) wins!" << std::endl;
+        hogwarts.removeStudent(student2->getName());
+        student2 = nullptr;
+        break;
+      }
+
+      damage = student2->attack();
+      if (damage > 0) {
+        student1->takeDamage(damage);
+        std::cout << student2->getName() << " [" << student2->getHealth()
+                  << "] "
+                  << " (Slytherin) attacks " << student1->getName()
+                  << " (Gryffindor) with " << student2->getPower()->getName()
+                  << " for " << damage << " damage" << std::endl;
+      }
+    }
+  }
+  
+  if (hogwarts.getHouseStudentsCount("Gryffindor") > 0) {
+    std::cout << "Gryffindor wins!" << std::endl;
+  }
+  else {
+    std::cout << "Slytherin wins!" << std::endl;
+  }
+  
+  return 0;
+}
