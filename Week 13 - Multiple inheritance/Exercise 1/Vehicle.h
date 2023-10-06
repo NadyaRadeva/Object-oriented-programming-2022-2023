bbbbcdbd3c8c0a25ef75a3b581755fc3833fd308
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 14:30:25 +0300

    Create Vehicle.h

---
+++ Week 13 - Multiple inheritance/Exercise 1/Vehicle.h
@@ -0,0 +1,19 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+class Vehicle {
+	int hp;
+	int weight;
+
+public:
+	Vehicle(int hp1, int weight1);
+
+	int getHp();
+	int getWeight();
+
+	virtual void startEngine() = 0;
+
+	virtual ~Vehicle();
+};
+
+#endif
