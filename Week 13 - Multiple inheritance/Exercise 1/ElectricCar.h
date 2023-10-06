fef831feff63ee3b1d1bd6a8cbd5065095b69de4
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 14:24:03 +0300

    Create ElectricCar.h

---
+++ Week 13 - Multiple inheritance/Exercise 1/ElectricCar.h
@@ -0,0 +1,13 @@
+#include "Car.h"
+
+#ifndef ELECTRICCAR_H
+#define ELECTRICCAR_H
+
+class ElectricCar : public Car {
+public:
+	ElectricCar(const char* str, int hp, int weight, const std::string& owner);
+
+	void startEngine();
+};
+
+#endif
