df3ff4ee8900cb644dfebcfc9935c5eb538d6e46
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 14:48:32 +0300

    Create Source.cpp

---
+++ Week 14 - Factory Design Pattern/Exercise - Grading/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Exam.h"
+
+int main() {
+	Exam exam;
+	QuestionFactory factory;
+
+	exam.addFromFactory(factory);
+	exam.addFromFactory(factory);
+	exam.addFromFactory(factory);
+	exam.addFromFactory(factory);
+
+	exam.answer();
+	exam.grade();
+	std::cout << exam.getPoints() << std::endl;
+}
