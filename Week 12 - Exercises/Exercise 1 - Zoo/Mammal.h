1e49f2725b52cdbbd32672a19312263662908a23
Nadya Radeva <116949953+NadyaRadeva@users.noreply.github.com>
2023-10-06 14:08:28 +0300

    Create Mammal.h

---
+++ Week 12 - Exercises/Exercise 1 - Zoo/Mammal.h
@@ -0,0 +1,26 @@
+#ifndef ZOO_MAMMAL_H
+#define ZOO_MAMMAL_H
+
+
+#include "Animal.h"
+
+class Mammal : public Animal{
+private:
+    char* nameOfAdopter;
+
+public:
+    Mammal(const char* name, int weight, Food foodType, const char* nameOfAdopter);
+    Mammal(const Mammal& other);
+    Mammal(Mammal&& other) noexcept ;
+    Mammal& operator=(const Mammal& other);
+    Mammal& operator=(Mammal&& other) noexcept ;
+
+    int eat() const override;
+
+    Food getFoodType() const override;
+
+    ~Mammal() override;
+};
+
+
+#endif //ZOO_MAMMAL_H
